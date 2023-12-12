fc6676d0d2e6e685dcf6170efbf3015d3b255173
bilawal-shah <32175026+bilawal-shah@users.noreply.github.com>
2023-12-12 12:41:13 +0500

    Delete stock-trader-app directory

--- stock-trader-app/src/routes.js
+++
@@ -1,9 +0,0 @@
-import Home from './components/myHome.vue'
-import portfolio from './components/Portfolio/myPortfolio.vue'
-import stocks from './components/Stocks/myStocks.vue'
-
-export const routes=[
-    {path:'/' , component:Home},
-    {path: '/portfolio', component:portfolio},
-    {path:'/stocks' , component: stocks}
-]
